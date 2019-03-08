a693f88ec48d2da402999f1ebf90cf63b7bb0559
VOLLETTE NEVEU EVAN <evollett@iut.local>
2019-03-08 15:41:08 +0100

    setup project

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+/*import 'waypoints/lib/noframework.waypoints.min.js'
+const sections = document.querySelectorAll('.-section')
+
+sections.forEach(section => {
+    const waypoint = new Waypoint({
+        element: section,
+        handler: function(direction) {
+            if(direction == "down") {
+                section.classList.add('-section--reached')
+            }
+            else {
+                section.classList.remove('-section--reached')
+            }
+            
+        },
+        offset: '2%'
+    })
+})*/
